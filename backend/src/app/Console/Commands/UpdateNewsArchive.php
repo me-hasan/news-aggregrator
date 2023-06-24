89475b3ed874ec73c12126e2cff29bd33f8b9f2d
khayrul <khayrul.web@gmail.com>
2023-06-24 14:03:14 +0600

    Update Update News Archive Command

--- backend/src/app/Console/Commands/UpdateNewsArchive.php
+++ backend/src/app/Console/Commands/UpdateNewsArchive.php
@@ -2,47 +2,33 @@
 
 namespace App\Console\Commands;
 
-namespace App\Console\Commands;
-
 use App\Jobs\PullNewsJob;
 use Illuminate\Console\Command;
 
-
-
-class PullNewsScheduled extends Command
+class UpdateNewsArchive extends Command
 {
-    /**
-     * The external api to consume the news api
-     * @var string
-     */
-    public $externalApi;
-
-    
-
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'schedule:pull-news';
+    protected $signature = 'update-news-archive:run';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Pull News from API';
+    protected $description = 'Pull news from API and update \'News Archive\' table';
 
     /**
      * Execute the console command.
      */
     public function handle()
-    {   
+    {
         $apiConfig = config('newssources.external_api');
         foreach($apiConfig as $key=> $config){
             PullNewsJob::dispatch(new $config['service']);
-        }
-        
-          
+        }  
     }
 }
